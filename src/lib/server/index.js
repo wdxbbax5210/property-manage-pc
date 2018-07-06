28674d78de8edac5e0c53f5e237a419cb74a4d11
郭文芳 <guowenfang@raycloud.com>
2018-07-06 20:56:43 +0800

    接口修改

--- src/lib/server/index.js
+++ src/lib/server/index.js
@@ -1,3 +1,3 @@
 import Fetch from '../fetch';
 //https://229492634.miss-xia-property-manage.club
-export let manage_upload_record = (...arg) => Fetch.post('/fee/record/upload/error', ...arg);
+export let manage_upload_record = (...arg) => Fetch.post('/upload/record', ...arg);
